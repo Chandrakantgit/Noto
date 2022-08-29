7d84d5f573856c96ac154151e3810fae7cea6b3f
Chandrakantgit <chandrakhannade@gmail.com>
2022-08-29 17:34:12 +0530

    initiated index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const port = process.env.port || 8000;
+const mongoose = require("mongoose");
+const app = express();
+const cookieParser = require("cookie-parser");
+
+// Database connections
+mongoose.connect(config.dbLink);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+mongoose.connection.on("error", (error) => {
+  console.log(error);
+});
+
+// Including important imp libraries
+app.use(express.json());
+app.use(cookieParser());
+
+// setting headers and checking authorization
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "z");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "authorization,userauth,Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  if (!req.headers.authorization) {
+    res.status(400);
+    console.log("check");
+    res.json({
+      status: "Forbidden",
+      message: "Authorization Missing",
+    });
+  } else {
+    if (auth.checkAuth(req.headers.authorization != 1)) {
+      res.status(401);
+      res.json({
+        status: "Forbidden",
+        message: "Authorization Wrong",
+      });
+    } else {
+      next();
+    }
+  }
+});
+
+// app.use("/auth", authRoutes);
+// app.use("/features", serviceRoutes);
+// app.use("/authNew", authRoutes2);
+
+app.listen(port, function () {
+  console.log("app is listing on " + port);
+});
